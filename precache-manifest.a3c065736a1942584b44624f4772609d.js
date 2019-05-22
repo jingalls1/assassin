bae57d0681797ef7ae4245d5d8a70084c18566cf
Jacob Ingalls <jingalls@uoregon.edu>
2019-05-22 15:46:49 -0700

    Updates

---
+++ precache-manifest.a3c065736a1942584b44624f4772609d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "21c518464a05461295ba104a393e1cde",
+    "url": "/assassin/index.html"
+  },
+  {
+    "revision": "6dd12485b91e78ade2af",
+    "url": "/assassin/static/css/main.c1c37d8b.chunk.css"
+  },
+  {
+    "revision": "9eee67cf782a38d35aeb",
+    "url": "/assassin/static/js/2.d6a534e5.chunk.js"
+  },
+  {
+    "revision": "6dd12485b91e78ade2af",
+    "url": "/assassin/static/js/main.f3457c88.chunk.js"
+  },
+  {
+    "revision": "ee7deeb8873c451a9c10",
+    "url": "/assassin/static/js/runtime~main.10bcc0a3.js"
+  }
+]);
